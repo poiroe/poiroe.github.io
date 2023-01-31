391fad746525a05951f75299a849781c1b9da585
poiroe <1129700720@qq.com>
2023-01-31 20:19:49 +0800

    1

---
+++ assets/js/tong.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?b287320452691eb59118e8edb42e5b55";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
