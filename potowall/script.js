1cd5842c8cf92d5eb1cccd1e0ddbb5f4bc7465fe
poiroe <1129700720@qq.com>
2023-11-22 05:27:50 +0800

    1

--- potowall/script.js
+++ potowall/script.js
@@ -1,33 +1,50 @@
 const galleryContainer = document.getElementById('galleryContainer');
 let page = 1;
 
-async function fetchImages() {
-  const response = await fetch(`https://api.unsplash.com/photos/?page=${page}&client_id=YOUR_UNSPLASH_API_KEY`);
+// 替换成你的GitHub仓库信息
+const repoOwner = 'poiroe';
+const repoName = 'poiroe.github.io';
+const folderPath = 'img';
+const githubToken = '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'; // 替换成你的GitHub个人访问令牌
+
+// 从GitHub获取图片链接的函数
+async function fetchImagesFromGitHub() {
+  // 替换成你实际的GitHub API调用
+  const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}?page=${page}&per_page=10`, {
+    headers: {
+      Authorization: `Bearer ${githubToken}`,
+    },
+  });
   const data = await response.json();
-  return data;
+  const imageUrls = data.map(file => file.download_url);
+  return imageUrls;
 }
 
-function createGalleryItem(photo) {
+// 创建图片元素的函数
+function createGalleryItem(imageUrl) {
   const galleryItem = document.createElement('div');
   galleryItem.classList.add('gallery-item');
 
   const img = document.createElement('img');
-  img.src = photo.urls.small;
-  img.alt = photo.alt_description;
+  // 懒加载属性
+  img.loading = 'lazy';
+  img.src = imageUrl;
+  // 根据需要添加alt文本
 
   galleryItem.appendChild(img);
   galleryContainer.appendChild(galleryItem);
 }
 
+// 加载图片的函数
 async function loadImages() {
-  const images = await fetchImages();
-  images.forEach(createGalleryItem);
+  const imageUrls = await fetchImagesFromGitHub();
+  imageUrls.forEach(createGalleryItem);
   page++;
 }
 
 loadImages();
 
-// Infinite scroll
+// 无限下拉
 window.addEventListener('scroll', () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
     loadImages();
