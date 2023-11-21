e4cf4275fe79de5cc30511c5ec2e8dc76ac5fa59
poiroe <1129700720@qq.com>
2023-11-22 05:36:48 +0800

    Update script.js

--- potowall/script.js
+++ potowall/script.js
@@ -4,20 +4,22 @@ let page = 1;
 // 替换成你的GitHub仓库信息
 const repoOwner = 'poiroe';
 const repoName = 'poiroe.github.io';
-const folderPath = 'img';
-const githubToken = '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'; // 替换成你的GitHub个人访问令牌
+const folderPath = 'img'; // 替换成你存放图片的文件夹路径
 
 // 从GitHub获取图片链接的函数
 async function fetchImagesFromGitHub() {
-  // 替换成你实际的GitHub API调用
-  const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}?page=${page}&per_page=10`, {
-    headers: {
-      Authorization: `Bearer ${githubToken}`,
-    },
-  });
-  const data = await response.json();
-  const imageUrls = data.map(file => file.download_url);
-  return imageUrls;
+  try {
+    const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}?page=${page}&per_page=10`);
+    const data = await response.json();
+    
+    // Extract image URLs
+    const imageUrls = data.map(file => file.download_url);
+
+    return imageUrls;
+  } catch (error) {
+    console.error('Error fetching images from GitHub:', error);
+    return [];
+  }
 }
 
 // 创建图片元素的函数
