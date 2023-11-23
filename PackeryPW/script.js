6996594a4cef2c1de8f1b5565703d5fdd5acfcdb
poiroe <1129700720@qq.com>
2023-11-24 05:22:06 +0800

    Update script.js

--- PackeryPW/script.js
+++ PackeryPW/script.js
@@ -13,7 +13,7 @@ async function fetchImagesFromGitHub() {
     
     const imageUrls = data.map(file => {
       const fileName = encodeURIComponent(file.name);
-      return `https://${repoOwner}.github.io/picx-images-hosting/${folderPath}/${fileName}`;
+      return `https://jsd.cdn.zzko.cn/gh/poiroe/picx-images-hosting/${folderPath}/${fileName}`;
     });
 
     return imageUrls;
