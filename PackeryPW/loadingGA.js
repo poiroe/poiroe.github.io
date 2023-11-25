7f85eff38bbd372258095378ebe4d870d143ef0f
poiroe <1129700720@qq.com>
2023-11-26 01:48:52 +0800

    V2

--- PackeryPW/loadingGA.js
+++ PackeryPW/loadingGA.js
@@ -1,11 +1,10 @@
-
-// loadingOverlay触发淡出倒计时 
+ 
  var loadingOverlay = document.getElementById('loadingOverlay');
   loadingOverlay.classList.add('fadeIn');
 
   setTimeout(function() {
     fadeOutLoadingOverlay();
-  }, 3500);
+  }, 2000);
 
 function fadeOutLoadingOverlay() {
   var loadingOverlay = document.getElementById('loadingOverlay');
@@ -16,21 +15,21 @@ function fadeOutLoadingOverlay() {
 // 延时后关闭加载动画
 setTimeout(() => {
   document.getElementById('loadingOverlay').style.display = 'none';
-}, 4000);
+}, 2500);
 
 setTimeout(() => {
   document.getElementById('loadingOverlayB').style.display = 'none';
-}, 2000);
+}, 1000);
 
 
 // 延时显示滚动条 overflow-Y: hidden
 setTimeout(() => {
   document.body.style.overflowY = 'visible';
-}, 4000);
+}, 2000);
 
 // 延迟显示body
 setTimeout(() => {
   const bodyDisplay = document.getElementById('bodydisplay');
   bodyDisplay.style.visibility = 'visible';
   bodyDisplay.style.opacity = '1';
-}, 4000);
+}, 2500);
--- PackeryPW/script.js
+++ PackeryPW/script.js
@@ -1,29 +1,6 @@
-const galleryContainer = document.getElementById('galleryContainer');
-let page = 1;
-let isLoading = false;
 
-const repoOwner = 'poiroe';
-const repoName = 'picx-images-hosting';
-let folderPath = 'VRChat';
 
-async function fetchImagesFromGitHub() {
-  try {
-    const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${folderPath}?page=${page}&per_page=10`);
-    const data = await response.json();
-    
-    const imageUrls = data.map(file => {
-      const fileName = encodeURIComponent(file.name);
-      return `https://cdn.jsdelivr.us/gh/poiroe/picx-images-hosting/${folderPath}/${fileName}`;
-    });
-
-    return imageUrls;
-  } catch (error) {
-    console.error('Error fetching images from GitHub:', error);
-    return [];
-  }
-}
-
-// 按钮点击事件监听器
+/* // 按钮点击事件监听器
 document.getElementById('switchToGame').addEventListener('click', async function() {
   console.log('Switching to Game-G');
   folderPath = 'Game-G';
@@ -65,16 +42,22 @@ document.addEventListener('DOMContentLoaded', function() {
 async function updateImageWall(images) {
   const galleryContainer = document.getElementById('galleryContainer');
 
-  // 获取所有现有的 grid__item 元素
-  const existingItems = galleryContainer.querySelectorAll('.grid__item');
+// 获取所有现有的 grid__sizer 元素
+const existingItems = galleryContainer.querySelectorAll('.grid__sizer');
 
-  // 删除所有现有的 grid__item 元素
-  existingItems.forEach(item => {
-    item.remove();
-  });
+// 跳过前三个元素
+const itemsToKeep = Array.from(existingItems).slice(1);
+
+// 清空现有元素的内容
+itemsToKeep.forEach(item => {
+  item.innerHTML = '';
+});
+
 
   // 删除所有 Fancybox 容器
   $('.fancybox__container').remove();
+
+
 
   // 遍历新的图片数组，创建新的 grid__item 元素
   images.forEach(imageUrl => {
@@ -85,12 +68,9 @@ async function updateImageWall(images) {
   Fancybox.bind('[data-fancybox]', {
         // Your custom options
       }); 
-}
+} */
 
-
-
-
-
+  // 动态颜色
 function changeLoadingRingColor() {
   const loadingRing = document.getElementById('loadingRing');
   let colorIndex = 0;
@@ -119,134 +99,13 @@ function changeLoadingRingColor() {
 }
 
 
-/* 创建属性 */
-function createGalleryItem(imageUrl, index) {
-  const gridItem = document.createElement('div');
-  gridItem.classList.add('grid__item', 'grid-item');
 
-  const link = document.createElement('a');
-  link.href = imageUrl;
-  link.setAttribute('data-fancybox', 'gallery');
-
-  const img = document.createElement('img');
-  img.loading = 'lazy'; // 添加 lazy loading
-  img.setAttribute('data-src', imageUrl);  // 存储真实图片路径到 data-src
-  img.src = 'https://pic.imgdb.cn/item/65610e96c458853aefb72b48.webp'; // 占位图路径
-
-  link.appendChild(img);
-  gridItem.appendChild(link);
-  galleryContainer.appendChild(gridItem);
-
-  if (index % 2 === 1) {
-    gridItem.classList.add('grid-item--width2');
-  }
-
-  // 添加监听器，当图片进入用户视野时触发加载
-  const observer = new IntersectionObserver(entries => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // 图片进入视野，将 data-src 替换为实际图片路径
-      img.src = img.getAttribute('data-src');
-      // 图片进入视野，触发布局
-      $grid.packery('layout');
-      // 停止监听，因为图片已经加载
-      observer.unobserve(img);
-    }
-  });
-}, { once: true });
-
-
-  // 开始监听
-  observer.observe(img);
-}
-
-
-async function loadImages() {
-  if (isLoading) {
-    return;
-  }
-
-  isLoading = true;
-
-  try {
-    const imageUrls = await fetchImagesFromGitHub();
-    const uniqueImageUrls = imageUrls.filter(url => !loadedImages.has(url));
-
-    uniqueImageUrls.forEach((imageUrl, index) => {
-      createGalleryItem(imageUrl, index);
-      const newItem = galleryContainer.lastElementChild;
-      newItem.style.transition = 'none';
-      loadedImages.add(imageUrl);
-    });
-
-    page++;
-
-    $grid = $('.grid').packery({
-      itemSelector: '.grid__item',
-      percentPosition: true
-    });
-
-$grid.imagesLoaded().done(function () {
-  console.log('All images have been successfully loaded!');
-  $grid.packery('layout');
-}).progress(function () {
-  console.log('An image has been loaded!');
-  $grid.packery('layout');
-});
-
-
-
-
-    setTimeout(() => {
-      galleryContainer.querySelectorAll('.grid__item').forEach(item => {
-        item.style.transition = '';
-      });
-    }, 0);
-  } finally {
-    isLoading = false;
-  }
-}
-
-// 保留已加载的图片的集合
-let loadedImages = new Set();
 
 // 颜色变换
 changeLoadingRingColor();
 
-// 初始化加载
-loadImages();
 
-// 延时后关闭加载动画
-setTimeout(() => {
-  document.getElementById('loadingOverlay').style.display = 'none';
-}, 5000);
-
-setTimeout(() => {
-  document.getElementById('loadingOverlayB').style.display = 'none';
-}, 3000);
-
-// 延时五秒后删除 overflow-Y: hidden
-setTimeout(() => {
-  document.body.style.overflowY = 'visible';
-}, 3500);
-
-// 在滚动到页面底部时调用 loadImages
-window.addEventListener('scroll', () => {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-    loadImages();
-  }
-});
-
-// 延迟显示body
-setTimeout(() => {
-  const bodyDisplay = document.getElementById('bodydisplay');
-  bodyDisplay.style.visibility = 'visible';
-  bodyDisplay.style.opacity = '1';
-}, 5000);
-
-
-
-setTimeout(function() {
+/* setTimeout(function() {
   var style = document.createElement('style');
   style.textContent = '@font-face {' +
     'font-family: "Bree Serif";' +
@@ -257,4 +116,4 @@ setTimeout(function() {
     'font-style: normal;' +
   '}';
   document.head.appendChild(style);
-}, 5000); // 在这里设置延迟的时间，单位为毫秒
+}, 5000); // 在这里设置延迟的时间，单位为毫秒 */
