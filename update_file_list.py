42f2947d82f8c0f92f46342235ff6f6b6b8eb221
poiroe <1129700720@qq.com>
2023-11-27 20:25:24 +0800

    1

---
+++ update_file_list.py
@@ -0,0 +1,38 @@
+import os
+import json
+import requests
+
+# GitHub repository information
+repo_owner = 'poiroe'
+repo_name = 'poiroe.github.io'
+MY_MY_MY_GITHUB_TOKEN = os.environ['MY_MY_GITHUB_TOKEN']
+
+# API endpoints for VRChat, Game-G, A&M
+api_endpoints = {
+    'VRChat': 'https://api.example.com/vrchat',
+    'Game-G': 'https://api.example.com/game-g',
+    'A&M': 'https://api.example.com/a&m'
+}
+
+# Destination directory in the GitHub repository
+destination_dir = 'PackeryPW/list'
+
+def fetch_and_save_to_json(api_name, api_endpoint):
+    resp = requests.get(api_endpoint)
+    data = resp.json()
+
+    json_filename = f'{destination_dir}/{api_name.lower()}.json'
+
+    with open(json_filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=2)
+
+# Loop through API endpoints and fetch data
+for api_name, api_endpoint in api_endpoints.items():
+    fetch_and_save_to_json(api_name, api_endpoint)
+
+# Push changes to GitHub repository
+os.system(f'git config user.email "actions@github.com"')
+os.system(f'git config user.name "GitHub Actions"')
+os.system(f'git add {destination_dir}/*.json')
+os.system(f'git commit -m "Update JSON files"')
+os.system(f'git push https://{MY_MY_MY_GITHUB_TOKEN}@github.com/{repo_owner}/{repo_name}.git HEAD:main')
