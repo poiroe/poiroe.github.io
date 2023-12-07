ca9558232e2caf142867e3def4ddbc5caa65afc9
poiroe <1129700720@qq.com>
2023-12-07 12:02:08 +0800

    1

--- PackeryPW/listAM.js
+++ PackeryPW/listAM.js
@@ -14,7 +14,7 @@ async function fetchImagesFromGitHub() {
     
     const imageUrls = data.map(file => {
       const fileName = encodeURIComponent(file.name);
-      return `https://jsd.cdn.zzko.cn/gh/poiroe/picx-images-hosting/${folderPath}/${fileName}`;
+      return `https://jsd.cdn.zzko.cn/gh/poiroe/picx-images-hosting@master/${folderPath}/${fileName}`;
     });
 
     return imageUrls;
